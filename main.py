63a6e278014192808d738e8a135736532ac76134
LudvigAnner <anner.ludvig@gmail.com>
2018-01-14 20:24:42 +0100

    Working

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from import_tools import import_tool
+from import_tools import data_import
 
 def main():
     import_tool = data_import()
     import_tool.save_nasdaq_stocklist('stockholm')
     import_tool.load_stocklist()
-    for import_tool.stocklist
-    import_tool.parse_stock_data(import_tool.stocklist[0, 1])
+    for el in import_tool.stocklist[:, 1]:
+        print('Saving ' + el)
+        import_tool.parse_stock_data(el)
 
     (timestamps, data) = import_tool.load_stock_data(import_tool.stocklist[0, 1])
 
